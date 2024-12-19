43e339c7c765b70281d4f0324a8213ef3f335174
Aishwaryendra Narayan <102553433+Aishwaryendra01@users.noreply.github.com>
2024-12-20 02:01:14 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+	//here program start
 public class DemoApplication {
 
 	public static void main(String[] args) {
